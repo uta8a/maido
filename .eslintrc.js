ff18da59827a853f36e510fb1fb4d331d23824cd
Masanori Tani <metro@uta8a.org>
2022-01-30 08:05:12 +0900

    Add next.js settings to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@next/next/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
