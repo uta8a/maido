5de69f3de26961f52384c08a2c04875768e5c6ad
Masanori Tani <metro@uta8a.org>
2022-01-30 08:15:08 +0900

    Add getMetadata.ts

---
+++ utils/getMetadata.ts
@@ -0,0 +1,22 @@
+import path from 'path';
+import fs from 'fs';
+import toml from 'toml';
+
+const documentRoot: string = 'content';
+const settingFilename: string = 'book_settings.toml';
+const defaultBookTitle: string = 'My Awesome TechBlog';
+
+// fullpathを得る、fileIO、file readの3つに分割できて、IO以外はテストが簡単なので外部関数に分割してもいいかも
+const getBookTitle = (basePath: string): string => {
+  const fullPath = path.join(basePath, documentRoot, settingFilename);
+  let fileRaw;
+  try {
+    fileRaw = fs.readFileSync(fullPath, 'utf8');
+  } catch (e) {
+    return defaultBookTitle;
+  }
+  const data = toml.parse(fileRaw); // TODO: ここtypeつけたい
+  return data.title;
+};
+
+export default getBookTitle;
