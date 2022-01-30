8a4884bcd5d388b727380858fa2e023155882287
Masanori Tani <metro@uta8a.org>
2022-01-30 09:29:15 +0900

    Add test, set default project title when no setting file

--- __tests__/utils/getMetadata.ts
+++ __tests__/utils/getMetadata.ts
@@ -3,3 +3,9 @@ import { getProjectTitle } from '../../utils/getMetadata';
 test('get project title from setting file', () => {
   expect(getProjectTitle(process.cwd())).toBe('My Super docs');
 });
+
+test('set default project title when no setting file', () => {
+  // './no_directory' doesn't exist.
+  // set defaultProjectTitle = 'My TechBlog'
+  expect(getProjectTitle('./no_directory')).toBe('My TechBlog');
+});
