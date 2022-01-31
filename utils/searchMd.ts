a9b33fee9a0f21aa66966fe35e64556689ae4bdf
Masanori Tani <metro@uta8a.org>
2022-01-31 10:05:19 +0900

    Change filtered out draft = true page

--- utils/searchMd.ts
+++ utils/searchMd.ts
@@ -1,5 +1,8 @@
 import path from 'path';
 import fs from 'fs';
+import { IndexRaw } from './types';
+import matter from 'gray-matter';
+import toml from 'toml';
 
 const searchMd = async (rootPath: string): Promise<string[]> => {
   const mdDirPaths: string[] = [];
@@ -11,7 +14,11 @@ const searchMd = async (rootPath: string): Promise<string[]> => {
         mdDirPaths.push(...mds);
       } else {
         if (checkMd(dirent.name)) {
-          mdDirPaths.push(path.join(rootPath, dirent.name));
+          const fullPath = path.join(rootPath, dirent.name);
+          const available = await checkAvailableMd(fullPath);
+          if (!available) {
+            mdDirPaths.push(fullPath);
+          }
         }
       }
     }
@@ -23,6 +30,29 @@ const searchMd = async (rootPath: string): Promise<string[]> => {
 
 const checkMd = (filepath: string): boolean => {
   return filepath.slice(-3) === '.md';
+};
+
+const checkAvailableMd = async (filepath: string): Promise<boolean> => {
+  let fileRaw;
+  try {
+    fileRaw = fs.readFileSync(filepath, 'utf-8');
+  } catch {
+    return false;
+  }
+  const data = getDraft(fileRaw);
+  return data;
+};
+
+// str === [book]/[...article].md's content
+const getDraft = (str: string): boolean => {
+  const rawData = matter(str, {
+    engines: {
+      toml: toml.parse.bind(toml),
+    },
+    language: 'toml',
+    delimiters: '+++',
+  });
+  return rawData.data.draft;
 };
 
 export { searchMd, checkMd };
