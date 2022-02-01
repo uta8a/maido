45cbbae04fea6dbc0bdf2a3871cf093f837d35ab
Masanori Tani <metro@uta8a.org>
2022-02-01 21:24:29 +0900

    Change select document root by env(DOCUMENT_ROOT) by using relative path

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,7 @@
-const documentRoot = 'content';
+const documentRoot =
+  process.env.DOCUMENT_ROOT !== undefined
+    ? process.env.DOCUMENT_ROOT
+    : 'content';
 const bookToc = 'toc.md';
 
 export { documentRoot, bookToc };
