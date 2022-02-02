17c5361d9bac5a8735b9b184e47e54094ceffb27
Masanori Tani <metro@uta8a.org>
2022-02-03 08:11:15 +0900

    Change style: BookCard's background-image changeable

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -15,7 +15,7 @@ const BookCard: FunctionComponent<Props> = (props: Props) => {
       href={props.linkPath}
       aria-label={props.title}
     >
-      <div className="w-full h-32 flex flex-col justify-between dark:bg-gray-800 bg-white hover:bg-yellow-100 dark:border-gray-700 rounded-lg border border-gray-400 py-5 px-4">
+      <div className="Z-bookcard-div w-full h-32 flex flex-col justify-between dark:bg-gray-800 bg-white dark:border-gray-700 rounded-lg border border-gray-400 py-5 px-4">
         <div>
           <h4 className="text-gray-800 dark:text-gray-100 font-bold mb-3">
             {props.title}
