bc83739de13eca96ea43051cf41026ef972d9fe9
Masanori Tani <metro@uta8a.org>
2022-02-05 17:47:15 +0900

    Formatted

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,28 @@ import ReactDOM from 'react-dom';
 import './style/main.scss';
 import { AppPage } from '@/App';
 import { HomePage } from './pages';
-import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
-      <Route path='/app' element={<AppPage />} />
-      <Route path='/' element={<HomePage books={[{title: 'book!', image_path: '/', date: '2022-01-18T05:28:15+09:00', book_path: '/'}]} projectTitle='title!'/>}/>
+      <Route path="/app" element={<AppPage />} />
+      <Route
+        path="/"
+        element={
+          <HomePage
+            books={[
+              {
+                title: 'book!',
+                image_path: '/',
+                date: '2022-01-18T05:28:15+09:00',
+                book_path: '/',
+              },
+            ]}
+            projectTitle="title!"
+          />
+        }
+      />
     </Routes>
   </BrowserRouter>,
   document.getElementById('root'),
