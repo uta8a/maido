80a8a18f8cec8b3b160c1a89dcca408bc8f46094
motoki saito <stmtk13044032@gmail.com>
2022-11-30 15:54:09 +0900

    tableタグにスタイル当てました

--- components/ArticleContent.tsx
+++ components/ArticleContent.tsx
@@ -6,12 +6,28 @@ type Props = {
 
 const ArticleContent: FunctionComponent<Props> = (props: Props) => {
   return (
-    <div
-      className="maido-content px-5 pb-20"
-      dangerouslySetInnerHTML={{
-        __html: props.content || '<div>No content.</div>',
-      }}
-    />
+    <>
+      <div
+        className="maido-content px-5 pb-20"
+        dangerouslySetInnerHTML={{
+          __html: props.content || '<div>No content.</div>',
+        }}
+      />
+      <style>
+        {`
+          .maido-content table {
+            border-collapse: collapse;
+            border: solid 2px black;
+          }
+          .maido-content table th, td {
+            border: solid 1px black;
+          }
+          .maido-content table tr:nth-child(2n) {
+            background-color: #ddd;
+          }
+        `}
+      </style>
+    </>
   );
 };
 
