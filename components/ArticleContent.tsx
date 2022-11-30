bea5eac9e012b5103d9962d5db9f56ab959c823b
motoki saito <stmtk13044032@gmail.com>
2022-11-30 18:07:17 +0900

    cssをスタイルシートのファイルに書き直した

--- components/ArticleContent.tsx
+++ components/ArticleContent.tsx
@@ -13,20 +13,6 @@ const ArticleContent: FunctionComponent<Props> = (props: Props) => {
           __html: props.content || '<div>No content.</div>',
         }}
       />
-      <style>
-        {`
-          .maido-content table {
-            border-collapse: collapse;
-            border: solid 2px black;
-          }
-          .maido-content table th, td {
-            border: solid 1px black;
-          }
-          .maido-content table tr:nth-child(2n) {
-            background-color: #ddd;
-          }
-        `}
-      </style>
     </>
   );
 };
